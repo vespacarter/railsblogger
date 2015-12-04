c66ee0109891ae389ce79ffcc845022a9c65b155
vespacarter <skasez@gmail.com>
2015-12-04 17:23:13 +0100

    adding comments iteration complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,8 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+		@comment = Comment.new
+		@comment.article_id = @article.id
 	end
 
 	def new
@@ -24,7 +26,7 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 		@article.destroy
 		flash.notice = "Article '#{@article.title}' Deleted!"
-		#redirect_to ni puta idea, el home
+		redirect_to articles_path
 	end
 
 	def edit
